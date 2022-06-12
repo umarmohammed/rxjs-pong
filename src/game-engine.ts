f19f5e44d9d3a8c91a04ec2dbcfbb72a67ba5401
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-12 12:54:49 +0100

    bugfix: rename move$ import in game-engine

--- src/game-engine.ts
+++ src/game-engine.ts
@@ -1,10 +1,10 @@
 import { scan } from "rxjs";
-import { movePaddleKey$ } from "./input";
+import { move$ } from "./input";
 import { renderState } from "./render";
 import { initialState, reducer, State } from "./state";
 
 export function startGame() {
-  const state$ = movePaddleKey$.pipe(scan(reducer, initialState));
+  const state$ = move$.pipe(scan(reducer, initialState));
 
   state$.subscribe(renderState);
 }
