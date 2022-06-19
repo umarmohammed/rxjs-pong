c5621a63014e847ba94893fca84175ef8f1de35b
Umar Mohammed <umar.mohammed@holisticai.com>
2022-06-19 11:39:58 +0100

    implement move when key held down

--- src/input.ts
+++ src/input.ts
@@ -1,9 +1,47 @@
-import { fromEvent, map, filter } from "rxjs";
-import { KeyPress, gameKeyPresses } from "./keypress.type";
+import {
+  fromEvent,
+  map,
+  filter,
+  takeUntil,
+  throttleTime,
+  repeat,
+  merge,
+} from "rxjs";
+import { KeyPress } from "./keypress.type";
 import { keyPressToMove } from "./move";
 
-export const move$ = fromEvent(window, "keyup").pipe(
+const leftMoveKeyup$ = fromEvent(window, "keyup").pipe(
   map((x) => (x as KeyboardEvent).key as KeyPress),
-  filter((x) => gameKeyPresses.includes(x)),
-  map((x) => keyPressToMove[x])
+  filter((x) => x === "q" || x === "a")
 );
+
+const leftMoveKeydown$ = fromEvent(window, "keydown").pipe(
+  map((x) => (x as KeyboardEvent).key as KeyPress),
+  filter((x) => x === "q" || x === "a"),
+  map((x) => keyPressToMove[x]),
+  throttleTime(50)
+);
+
+const rightMoveKeyup$ = fromEvent(window, "keyup").pipe(
+  map((x) => (x as KeyboardEvent).key as KeyPress),
+  filter((x) => x === "o" || x === "l")
+);
+
+const rightMoveKeydown$ = fromEvent(window, "keydown").pipe(
+  map((x) => (x as KeyboardEvent).key as KeyPress),
+  filter((x) => x === "o" || x === "l"),
+  map((x) => keyPressToMove[x]),
+  throttleTime(50)
+);
+
+const moveLeftPaddle$ = leftMoveKeydown$.pipe(
+  takeUntil(leftMoveKeyup$),
+  repeat()
+);
+
+const moveRightPaddle$ = rightMoveKeydown$.pipe(
+  takeUntil(rightMoveKeyup$),
+  repeat()
+);
+
+export const move$ = merge(moveLeftPaddle$, moveRightPaddle$);
